70911a2fb8ba0e7a03704940e039a3d9cbb6ed80
chenghh <chenghh@celuechaogu.com>
2017-02-17 00:01:08 +0800

    添加分治 二分查找 和 乘方算法。

---
+++ introductionToAlgorithm/divideconquer/binarysearch.py
@@ -0,0 +1,12 @@
+#encoding:utf-8
+
+
+
+def binarysearch(sortedlist,x):
+    """
+    二分查找.分治实现. 时间复杂度lgn
+    :param sortedlist: 已排好序的数字列表
+    :param x: 待查找的元素x
+    :return: x所在下标,如果不存在,返回-1
+    """
+    #TODO 待实现
---
+++ introductionToAlgorithm/divideconquer/involution.py
@@ -0,0 +1,11 @@
+#encoding:utf-8
+
+def involution(num,times):
+    """
+    计算num的times次方. 分治法实现. 时间复杂度lgn
+    考虑times为奇数或偶数时的处理.
+    :param num:
+    :param times:
+    :return:
+    """
+    pass #TODO 待实现
